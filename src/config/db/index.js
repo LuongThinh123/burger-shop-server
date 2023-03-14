29178202565797cc782074946b48f28de06582d4
LuongThinh123 <luongchithinh123@gmail.com>
2023-03-14 17:24:58 +0700

    change mongodb url

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+// mongodb://127.0.0.1:27017/food-shop url to connect
 
+// mongodb://localhost:27017 mongodb connection name
 async function connect() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/food-shop", {
+    await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
